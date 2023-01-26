454cfe7ac44514afdbcc9b851690eba82e23eb82
Isaac Olowookere <olowookere.isaaco@gmail.com>
2023-01-27 00:50:21 +0100

    Modified package.json and server.ts files

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -39,6 +39,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
     res.status(200).sendFile(filtered_image, () => {
       deleteLocalFiles([filtered_image]);
     })
+
   })
   //! END @TODO1
 
